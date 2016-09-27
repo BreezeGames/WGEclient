dc6acd73471407276edb9e05f4b4dc384bd320af
Георгий Попов <popoff96@live.com>
2016-09-28 01:00:11 +0300

    Grunt and Npm connected to the project  - Grunt make build/wgecore.js file - uglified and concatenated version  - NPM package.json downloads all dev dependencies

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+
+module.exports = function(grunt) {
+    "use strict";
+
+    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        concat: {
+            main: {
+                src: [
+                    "scripts/core/*.js"
+                ],
+                dest: "build/wgecore.js"
+            }
+        },
+        uglify: {
+            main: {
+                files: {
+                    "build/wgecore.js": "build/wgecore.js"
+                }
+            }
+        },
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: [
+                    "babel-preset-es2015"
+                ]
+            },
+            dist: {
+                files: {
+                    "build/wgecore.js": "<%=  concat.main.dest %>"
+                }
+            }
+        }
+    });
+
+    grunt.registerTask("default", ["concat", "babel", "uglify"]);
+    grunt.registerTask("dev", ["concat", "babel"]);
+};
